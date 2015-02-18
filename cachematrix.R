e8d27645d94789d6af5b157ac73ff989898427d6
Lonny Hurwitz <Lonny28@users.noreply.github.com>
2015-02-17 21:35:04 -0600

    Cleaning up comments
    Getting ready to do real work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment 2, where I will attempt to write
+## two functions.  The first will Cache the mean of a vector,
+## the second will cache the inverse of the matrix from the first function.
 
-## Write a short comment describing this function
+## Here is the first function, called makeCacheMatrix
+## I'll add more comments later as it grows
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## The second function cache solve follows.
+## I'll add more code as I go
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-
-
-##this is a test edit to Lonny's code to see if it updates correctly
